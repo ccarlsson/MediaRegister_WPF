e76dfde7a49759685f53a98ff562d3b19e26999d
Christer Carlsson <ccarlsson@outlook.com>
2023-12-15 21:41:42 +0100

    Add EnsureCreated

--- MediaRegister.gRPC/Program.cs
+++ MediaRegister.gRPC/Program.cs
@@ -7,12 +7,20 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddGrpc(); 
 builder.Services.AddDbContext<AppDbContext>(options 
-    => options.UseSqlite("Data Source=./db/media.db"));
+    => options.UseSqlite("Data Source=media.db"));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
 app.MapGrpcService<MediaService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+// Ensure the database is created.
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
+    context.Database.EnsureCreated();
+}
 
 app.Run();
